d969c27296833a73bf191377550d0aac8b21c9e7
Hotel-Management-System <umeshlnayak@gmail.com>
2025-03-11 15:53:07 +0530

    error correction

--- controllers/user.js
+++ controllers/user.js
@@ -36,7 +36,7 @@ exports.postLogin = (req, res, next) => {
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
 
@@ -74,7 +74,7 @@ exports.postCreateAccount = (req, res, next) => {
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
 
@@ -108,7 +108,7 @@ exports.postCategory = (req, res, next) => {
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
 
@@ -143,7 +143,7 @@ exports.postStatus = (req, res, next) => {
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
    var date = req.body.date;
@@ -177,7 +177,7 @@ exports.getShowStatus = (req, res, next) => {
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
 
@@ -211,7 +211,7 @@ exports.deleteBooking =(req,res,next)=>{
    var connectDB = mysql.createConnection({
       host: "localhost",
       user: "root",
-      password: "",
+      password: "ULN@NAYAK879",
       database: "hotel"
    });
 
